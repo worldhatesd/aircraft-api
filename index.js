890c2be795930510ad897e99c62aa748a25bdc00
Deonte Palmer <ferfrink3577@gmail.com>
2024-06-15 21:34:59 -0500

    feat: Add Electron integration for desktop application

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require("express"),
     port = process.env.PORT || 3000,
     bodyParser = require("body-parser"),
     api = require("./api");
+const { App, BrowserWindow } = require("electron");
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
